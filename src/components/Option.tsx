37c81a0cec6865f00b6359319e7142f8767e5c46
Segundavid-Dev <segdavid03@gmail.com>
2025-05-01 12:43:34 +0100

    style: completed welcome page

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -2,20 +2,46 @@ import { Link } from "react-router-dom";
 
 export default function Option() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to={"/HTML"}>HTML</Link>
-        </li>
-        <li>
-          <Link to={"/CSS"}>CSS</Link>
-        </li>
-        <li>
-          <Link to={"/Javascript"}>Javascript</Link>
-        </li>
-        <li>
-          <Link to={"/Accessibility"}>Accessibility</Link>
-        </li>
+    <div className="">
+      <ul className="">
+        <Link to={"/HTML"}>
+          <li className="flex items-center mb-5 pr-60 py-3 pl-3 bg-[var(--option-bg)] rounded-2xl">
+            <span className="bg-[var(--html-background)] p-2 rounded-lg">
+              <img src="/images/icon-html.svg" alt="Html icon" />
+            </span>
+            <span className="ml-3">HTML</span>
+          </li>
+        </Link>
+
+        <Link to={"/CSS"}>
+          <li className="flex items-center mb-5 pr-60 py-3 pl-3 bg-[var(--option-bg)] rounded-2xl">
+            <span className="bg-[var(--css-background)] p-2 rounded-lg">
+              <img src="/images/icon-css.svg" alt="CSS icon" />
+            </span>
+            <span className="ml-3">CSS</span>
+          </li>
+        </Link>
+
+        <Link to={"/Javascript"}>
+          <li className="flex items-center mb-5 pr-60 py-3 pl-3 bg-[var(--option-bg)] rounded-2xl">
+            <span className="bg-[var(--javascript-background)] p-2 rounded-lg">
+              <img src="/images/icon-js.svg" alt="Javascript icon" />
+            </span>
+            <span className="ml-3">Javascript</span>
+          </li>
+        </Link>
+
+        <Link to={"/Accessibility"}>
+          <li className="flex items-center mb-5 pr-60 py-3 pl-3 bg-[var(--option-bg)] rounded-2xl">
+            <span className="bg-[var(--accessibility-background)] p-2 rounded-lg">
+              <img
+                src="/images/icon-accessibility.svg"
+                alt="Accessibility icon"
+              />
+            </span>
+            <span className="ml-3">Accessibility</span>
+          </li>
+        </Link>
       </ul>
     </div>
   );
