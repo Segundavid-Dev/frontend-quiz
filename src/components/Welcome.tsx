8d65629ad6fa05b5e863deb6e7d17a254d099d55
Segundavid-Dev <segdavid03@gmail.com>
2025-05-01 09:45:53 +0100

    chore: install react router

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,8 +1,12 @@
 export default function Welcome() {
   return (
     <div>
-      <h1>Welcome to the Frontend Quiz</h1>
-      <p>Pick a subject to get started</p>
+      <h1 className="text-6xl">
+        Welcome to the <br /> <span className="font-bold">Frontend Quiz!</span>
+      </h1>
+      <p className="text-[14px] italic text-gray-400 mt-5">
+        Pick a subject to get started.
+      </p>
     </div>
   );
 }
